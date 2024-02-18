93b24205ddb92d3160b7252a8cfb61c7d0fa86c8
gyj <gyj@gmail.com>
2024-02-18 14:38:13 +0900

    프로젝트시작

---
+++ src/main/java/web/goproject/GoprojectApplication.java
@@ -0,0 +1,13 @@
+package web.goproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/goproject/GoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package web.goproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
